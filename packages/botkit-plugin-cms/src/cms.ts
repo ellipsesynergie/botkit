a9b3d9a25d8b002fa1c66ea6b23fec1a80d5e31e
Etienne <etienne.bolduc@ellipse-synergie.com>
2019-11-07 15:44:04 -0500

    Add local CMS plugin (#2)

--- packages/botkit-plugin-cms/src/cms.ts
+++ packages/botkit-plugin-cms/src/cms.ts
@@ -6,8 +6,10 @@
  * Licensed under the MIT License.
  */
 
-import { Botkit, BotkitDialogWrapper, BotkitMessage, BotWorker, BotkitConversation } from 'botkit';
+import { Botkit, BotkitDialogWrapper, BotkitMessage, BotWorker, BotkitConversation, BotkitPlugin } from 'botkit';
 import * as request from 'request';
+import { CmsPluginCore } from './plugin-core';
+
 const url = require('url');
 const debug = require('debug')('botkit:cms');
 
@@ -27,21 +29,23 @@ const debug = require('debug')('botkit:cms');
  * });
  * ```
  */
-export class BotkitCMSHelper {
+export class BotkitCMSHelper extends CmsPluginCore implements BotkitPlugin {
     private _config: any;
-    private _controller: Botkit;
+    protected _controller: Botkit;
 
     /**
      * Botkit Plugin name
      */
-    public name: string = 'Botkit CMS';
+    public name = 'Botkit CMS';
 
     public constructor(config: CMSOptions) {
+        super();
+
         this._config = config;
         if (config.controller) {
             this._controller = this._config.controller;
         }
-        
+
         // for backwards compat, handle these alternate locations
         if (this._config.cms_uri && !this._config.uri) {
             this._config.uri = this._config.cms_uri;
@@ -79,8 +83,8 @@ export class BotkitCMSHelper {
         });
     }
 
-    private async apiRequest(uri: string, params: {[key: string]: any} = {}, method: string = 'GET'): Promise<any> {
-        let req = {
+    private async apiRequest(uri: string, params: {[key: string]: any} = {}, method = 'GET'): Promise<any> {
+        const req = {
             uri: url.resolve(this._config.uri, uri + '?access_token=' + this._config.token),
             headers: {
                 'content-type': 'application/json'
@@ -136,104 +140,29 @@ export class BotkitCMSHelper {
 
     /**
      * Load all script content from the configured CMS instance into a DialogSet and prepare them to be used.
-     * @param dialogSet A DialogSet into which the dialogs should be loaded.  In most cases, this is `controller.dialogSet`, allowing Botkit to access these dialogs through `bot.beginDialog()`.
+     * @param botkit The Botkit controller instance
      */
     public async loadAllScripts(botkit: Botkit): Promise<void> {
-        var scripts = await this.getScripts();
+        const scripts = await this.getScripts();
 
         scripts.forEach((script) => {
             // map threads from array to object
-            let threads = {};
+            const threads = {};
             script.script.forEach((thread) => {
                 threads[thread.topic] = thread.script.map(this.mapFields);
             });
 
-            let d = new BotkitConversation(script.command, this._controller);
-            d.script = threads;
-            botkit.addDialog(d);
+            const dialog = new BotkitConversation(script.command, this._controller);
+            dialog.script = threads;
+            botkit.addDialog(dialog);
         });
-    }
-
-    /**
-     * Map some less-than-ideal legacy fields to better places
-     */
-    private mapFields(line): void {
-        // Create the channelData field where any channel-specific stuff goes
-        if (!line.channelData) {
-            line.channelData = {};
-        }
-
-        // TODO: Port over all the other mappings
-
-        // move slack attachments
-        if (line.attachments) {
-            line.channelData.attachments = line.attachments;
-        }
-
-        // we might have a facebook attachment in fb_attachments
-        if (line.fb_attachment) {
-            let attachment = line.fb_attachment;
-            if (attachment.template_type) {
-                if (attachment.template_type === 'button') {
-                    attachment.text = line.text[0];
-                }
-                line.channelData.attachment = {
-                    type: 'template',
-                    payload: attachment
-                };
-            } else if (attachment.type) {
-                line.channelData.attachment = attachment;
-            }
-
-            // blank text, not allowed with attachment
-            line.text = null;
-
-            // remove blank button array if specified
-            if (line.channelData.attachment.payload.elements) {
-                for (var e = 0; e < line.channelData.attachment.payload.elements.length; e++) {
-                    if (!line.channelData.attachment.payload.elements[e].buttons || !line.channelData.attachment.payload.elements[e].buttons.length) {
-                        delete (line.channelData.attachment.payload.elements[e].buttons);
-                    }
-                }
-            }
-
-            delete (line.fb_attachment);
-        }
-
-        // Copy quick replies to channelData.
-        // This gives support for both "native" quick replies AND facebook quick replies
-        if (line.quick_replies) {
-            line.channelData.quick_replies = line.quick_replies;
-        }
-
-        // handle teams attachments
-        if (line.platforms && line.platforms.teams) {
-            if (line.platforms.teams.attachments) {
-                line.attachments = line.platforms.teams.attachments.map((a) => {
-                    a.content = { ...a };
-                    a.contentType = 'application/vnd.microsoft.card.' + a.type;
-                    return a;
-                });
-            }
-            delete (line.platforms.teams);
-        }
-
-        // handle additional custom fields defined in Botkit-CMS
-        if (line.meta) {
-            for (var a = 0; a < line.meta.length; a++) {
-                line.channelData[line.meta[a].key] = line.meta[a].value;
-            }
-            delete (line.meta);
-        }
-
-        return line;
     }
 
     /**
      * Uses the Botkit CMS trigger API to test an incoming message against a list of predefined triggers.
      * If a trigger is matched, the appropriate dialog will begin immediately.
      * @param bot The current bot worker instance
-     * @param message An incoming message to be interpretted
+     * @param message An incoming message to be interpreted
      * @returns Returns false if a dialog is NOT triggered, otherwise returns void.
      */
     public async testTrigger(bot: BotWorker, message: Partial<BotkitMessage>): Promise<any> {
@@ -242,82 +171,6 @@ export class BotkitCMSHelper {
             return await bot.beginDialog(command.command);
         }
         return false;
-    }
-
-    /**
-     * Bind a handler function that will fire before a given script and thread begin.
-     * Provides a way to use BotkitConversation.before() on dialogs loaded dynamically via the CMS api instead of being created in code.
-     *
-     * ```javascript
-     * controller.cms.before('my_script','my_thread', async(convo, bot) => {
-     *
-     *  // do stuff
-     *  console.log('starting my_thread as part of my_script');
-     *  // other stuff including convo.setVar convo.gotoThread
-     *
-     * });
-     * ```
-     *
-     * @param script_name The name of the script to bind to
-     * @param thread_name The name of a thread within the script to bind to
-     * @param handler A handler function in the form async(convo, bot) => {}
-     */
-    public before(script_name: string, thread_name: string, handler: (convo: BotkitDialogWrapper, bot: BotWorker) => Promise<void>): void {
-        let dialog = this._controller.dialogSet.find(script_name) as BotkitConversation;
-        if (dialog) {
-            dialog.before(thread_name, handler);
-        } else {
-            throw new Error('Could not find dialog: ' + script_name);
-        }
-    }
-
-    /**
-     * Bind a handler function that will fire when a given variable is set within a a given script.
-     * Provides a way to use BotkitConversation.onChange() on dialogs loaded dynamically via the CMS api instead of being created in code.
-     *
-     * ```javascript
-     * controller.plugins.cms.onChange('my_script','my_variable', async(new_value, convo, bot) => {
-    *
-    * console.log('A new value got set for my_variable inside my_script: ', new_value);
-    *
-    * });
-    * ```
-    *
-    * @param script_name The name of the script to bind to
-    * @param variable_name The name of a variable within the script to bind to
-    * @param handler A handler function in the form async(value, convo, bot) => {}
-    */
-    public onChange(script_name: string, variable_name: string, handler: (value: any, convo: BotkitDialogWrapper, bot: BotWorker) => Promise<void>): void {
-        let dialog = this._controller.dialogSet.find(script_name) as BotkitConversation;
-        if (dialog) {
-            dialog.onChange(variable_name, handler);
-        } else {
-            throw new Error('Could not find dialog: ' + script_name);
-        }
-    }
-
-    /**
-    * Bind a handler function that will fire after a given dialog ends.
-    * Provides a way to use BotkitConversation.after() on dialogs loaded dynamically via the CMS api instead of being created in code.
-    *
-    * ```javascript
-    * controller.plugins.cms.after('my_script', async(results, bot) => {
-    *
-    * console.log('my_script just ended! here are the results', results);
-    *
-    * });
-    * ```
-    *
-    * @param script_name The name of the script to bind to
-    * @param handler A handler function in the form async(results, bot) => {}
-    */
-    public after(script_name: string, handler: (results: any, bot: BotWorker) => Promise<void>): void {
-        let dialog = this._controller.dialogSet.find(script_name) as BotkitConversation;
-        if (dialog) {
-            dialog.after(handler);
-        } else {
-            throw new Error('Could not find dialog: ' + script_name);
-        }
     }
 }
 
